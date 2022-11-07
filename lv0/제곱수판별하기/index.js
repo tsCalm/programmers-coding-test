c5100e167d253c39a4461ddfb049fa899d2a45cc
tsCalm <dlatkdals93@gmail.com>
2022-11-08 03:00:08 +0900

    feat: 제곱수판별하기

---
+++ lv0/제곱수판별하기/index.js
@@ -0,0 +1,11 @@
+/**
+ * 문제 설명: 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+ * 제한사항
+1 ≤ n ≤ 1,000,000
+ */
+
+function solution(n) {
+  var answer = Math.sqrt(n);
+
+  return Number.isInteger(answer) ? 1 : 2;
+}
