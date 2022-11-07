2f1bc205be1ce6c99f631d05cb2ed5dc2156665a
tsCalm <dlatkdals93@gmail.com>
2022-11-08 03:00:47 +0900

    feat: 문자열안에문자열

---
+++ lv0/문자열안에문자열/index.js
@@ -0,0 +1,11 @@
+/**
+ * 문제 설명: 문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+ * 제한사항
+1 ≤ str1의 길이 ≤ 100
+1 ≤ str2의 길이 ≤ 100
+ */
+
+function solution(str1, str2) {
+  const answer = str1.includes(str2);
+  return answer ? 1 : 2;
+}
