d85c51bfada8fec69a042c8f2dfcba8e12676bdf
tsCalm <dlatkdals93@gmail.com>
2022-11-08 03:39:43 +0900

    feat: 특이한정렬

---
+++ lv0/특이한정렬/index.js
@@ -0,0 +1,39 @@
+/**
+ * 문제 설명: 정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다. 이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다. 
+ * 정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
+ * 
+ * 제한사항
+1 ≤ n ≤ 10,000
+1 ≤ numlist의 원소 ≤ 10,000
+1 ≤ numlist의 길이 ≤ 100
+numlist는 중복된 원소를 갖지 않습니다.
+ */
+function solution(numlist, n) {
+  numlist.sort((a, b) => a - b);
+  const answer = [
+    // {
+    //   value: n,
+    //   absValue: n,
+    //   order: 0,
+    // },
+  ];
+  numlist.forEach((num) => {
+    const minusNum = n - num;
+    const result = {
+      value: num,
+      absValue: Math.abs(minusNum),
+      order: minusNum > 0 ? 1 : -1,
+    };
+    answer.push(result);
+  });
+
+  answer.sort((a, b) => {
+    const aAbsV = a.absValue;
+    const bAbsV = b.absValue;
+    if (aAbsV === bAbsV) {
+      return a.order - b.order;
+    }
+    return aAbsV - bAbsV;
+  });
+  return answer.map((obj) => obj.value);
+}
