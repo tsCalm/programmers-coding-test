ca291fecb2c374e94bc35342f944011aca3ff2f3
tsCalm <dlatkdals93@gmail.com>
2022-11-08 03:03:02 +0900

    feat: 자릿수더하기

---
+++ lv0/자릿수더하기/index.js
@@ -0,0 +1,10 @@
+/**
+ * 문제 설명: 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+ * 제한사항
+0 ≤ n ≤ 1,000,000
+ */
+
+function solution(n) {
+  let answer = n.toString().split("");
+  return answer.reduce((acc, cur) => acc + Number(cur), 0);
+}
