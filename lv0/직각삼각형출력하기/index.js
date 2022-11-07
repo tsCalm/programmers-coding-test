8e0f2faf9e925790639b2f87676ba278b579fc57
tsCalm <dlatkdals93@gmail.com>
2022-11-08 03:10:16 +0900

    feat: 직각삼각형출력하기

---
+++ lv0/직각삼각형출력하기/index.js
@@ -0,0 +1,27 @@
+/**
+ * 문제 설명: "*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
+ * 제한사항
+1 ≤ n ≤ 10
+ */
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const num = Number(input[0]);
+  let logStr = "";
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j <= i; j++) {
+      logStr += "*";
+    }
+    logStr += "\n";
+  }
+  console.log(logStr);
+});
