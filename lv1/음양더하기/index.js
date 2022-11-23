4ca626b3f5405be4e2592587cff3923608f64813
tsCalm <dlatkdals93@gmail.com>
2022-11-23 16:26:39 +0900

    feat: lv1 - 음양더하기

---
+++ lv1/음양더하기/index.js
@@ -0,0 +1,24 @@
+/**
+ * 문제설명 :
+ * 어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다.
+ * 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항 :
+ * absolutes의 길이는 1 이상 1,000 이하입니다.
+ * absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
+ * signs의 길이는 absolutes의 길이와 같습니다.
+ * signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
+ */
+
+function solution(absolutes, signs) {
+  const answer = absolutes.map((num, idx) => {
+    if (signs[idx]) return num;
+    return num * -1;
+  });
+  return answer.reduce((prev, cur) => prev + cur, 0);
+}
+const param = [4, 7, 12];
+const param2 = [true, false, true];
+const result = solution(param, param2);
+
+console.log("result : ", result);
